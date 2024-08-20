73f526ac9f02e33a77486b29d45a1da3dd1f424d
Daniyal Bekinalkar <daniyal.bekinalkar@gmail.com>
2024-08-20 14:48:30 -0500

    use helper function to finish coding fizzbuzz

--- first_steps/fizzbuzz.exs
+++ first_steps/fizzbuzz.exs
@@ -1,11 +1,17 @@
-fizzbuzz = fn
+helper = fn
   0, 0, _t  -> "FizzBuzz"
   0, _, _ -> "Fizz"
   _, 0, _ -> "Buzz"
   _, _, v -> "#{v}"
 end
 
-IO.puts fizzbuzz.(0, 0, 1)
-IO.puts fizzbuzz.(0, 1, 1)
-IO.puts fizzbuzz.(1, 0, 1)
-IO.puts fizzbuzz.(1, 1, 1)
+fizzbuzz = fn
+  n -> helper.(rem(n, 3), rem(n, 5), n)
+end
+
+IO.puts fizzbuzz.(10)
+IO.puts fizzbuzz.(11)
+IO.puts fizzbuzz.(12)
+IO.puts fizzbuzz.(13)
+IO.puts fizzbuzz.(14)
+IO.puts fizzbuzz.(15)
